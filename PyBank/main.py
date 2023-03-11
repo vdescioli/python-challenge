c969d526d164cd90d9ad71a6dea84622e243bec1
vdescioli <vdescioli@gmail.com>
2023-03-11 14:09:28 -0500

    Adding Greatest Increase and Decrease

--- PyBank/main.py
+++ PyBank/main.py
@@ -6,6 +6,7 @@ budget_csv = os.path.join("..", "Resources", "budget_data.csv")
 # Lists to store data and variables
 month = []
 profit = []
+monthly_change = []
 month_total = 0
 profit_total = 0
 
@@ -21,6 +22,15 @@ with open(budget_csv) as csvfile:
         profit.append(int(row[1])) 
         profit_total = profit_total + int(row[1])
 
-print(f"Total Months:  " + str(month_total))
-print(f"Total:  $" + str(profit_total))
+    # create monthly change list and find min and max 
+    for i in range(1, len(profit)-1):
+        monthly_change.append(profit[i] - profit[i-1])
+        
+    g_inc_index = monthly_change.index(max(monthly_change))
+    g_dec_index = monthly_change.index(min(monthly_change))        
+                
 
+print("Total Months:  " + str(month_total))
+print("Total:  $" + str(profit_total))
+print("Greatest Increase in Profits:  ",month[g_inc_index + 1], "  $", max(monthly_change))
+print("Greatest Decrease in Profits:  ", month[g_dec_index+1], "  $", min(monthly_change))
