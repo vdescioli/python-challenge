8e1abb18448c8041f7076894791c63c262b79c2d
vdescioli <vdescioli@gmail.com>
2023-03-11 18:45:31 -0500

    Moved 2nd loop to print candidate info properly and added winner

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -3,7 +3,7 @@ import csv
 
 election_csv = os.path.join("..", "Resources", "election_data.csv")
 
-# List for candiadtes, dictionary for votes, and variables
+# Lists, dictionaries, and variables
 candidates = []
 v_per_cand = {}
 percentage = {}
@@ -23,13 +23,20 @@ with open(election_csv) as csvfile:
             candidates.append(row[2])
             v_per_cand[row[2]] = 0
         v_per_cand[row[2]] += 1
+
+# start printing output 
+print(" ")
+print("Election Results")
+print("-------------------------")
+print("Total Votes:  ", vote_total)
+print("-------------------------")    
     
-    # calculate percentage of total votes 
-    for i in candidates:
-        percentage[i] = v_per_cand[i]/vote_total
-        
-        
-print(vote_total)
-print(candidates)
-print(v_per_cand)
-print(percentage)
+    # calculate percentage of total votes and print all candidate info 
+for i in candidates:
+    percentage[i] = (v_per_cand[i]/vote_total)*100
+    print(i +":  " + '{:.3f}%'.format(percentage[i]) + ' ({:.0f})'.format(v_per_cand[i]))
+    
+# Winner of Election 
+print("-------------------------")
+print("Winner: " + max(v_per_cand, key=v_per_cand.get))
+print("-------------------------")
